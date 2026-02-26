49fe7c62db67e9fe1a100b1f638d74c2c4c5fe92
Mitja Sainio <mitjasai@csc.fi>
2026-02-26 15:28:01 +0200

    Fix target path in get_lumi_data.sh

--- scripts/get_lumi_data.sh
+++ scripts/get_lumi_data.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Exit setup if any command fails
-set -e
+#
+# Get ImageNet training data for LUMI AI guide PyTorch examples.
+# This only works on LUMI, since the ImageNet dataset is made available there.
+#
 
-# Get training data for PyTorch tests. This only works on LUMI, since the ImageNet dataset is
-# already available there.
-tiny_imagenet_dataset=/appl/local/training/LUMI-AI-Guide/tiny-imagenet-dataset.hdf5
-train_images=data/pytorch/train_images.hdf5
-if [ -f $tiny_imagenet_dataset ] && [ ! -f $train_images ]; then
-    mkdir -p data/pytorch
-    cp $tiny_imagenet_dataset $train_images
+imagenet_source=/appl/local/training/LUMI-AI-Guide/tiny-imagenet-dataset.hdf5
+imagenet_target=benchmarks/pytorch/train_images.hdf5
+
+if [ -f $imagenet_source ] && [ ! -f $imagenet_target ]; then
+    cp $imagenet_source $imagenet_target
 fi
