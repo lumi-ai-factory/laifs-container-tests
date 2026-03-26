d6cd18f348beabd28843fd7b2f891acfe1d7c926
Mitja Sainio <mitjasai@csc.fi>
2026-03-26 10:32:54 +0200

    Load environment module in single line

--- scripts/test_runner.sh
+++ scripts/test_runner.sh
@@ -3,8 +3,7 @@
 SIF_PATH=$1
 
 module --quiet purge
-module use /appl/local/laifs/modules
-module load lumi-aif-singularity-bindings
+module load Local-LAIF lumi-aif-singularity-bindings
 
 source .virtualenvs/runner/bin/activate
 
